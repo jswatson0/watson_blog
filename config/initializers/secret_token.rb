dfa5c2c410012f3aad2dfc314c76b6dba4619adf
Jason Watson <jswatson0@gmail.com>
2013-07-01 10:45:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatsonBlog::Application.config.secret_token = 'ecde3e9ba91d80a6aa184d233e975a2ab596c6355acc2a0c85d308122bd6a4c14d8fd10849077b6602db8ef7708bca1e21fd4d1347cf7501cd95ee7d359616e5'
